305e3c4eec0513265153a0b602feddd6a3bbef61
rzryr1 <solotogre@gmail.com>
2024-12-24 15:03:18 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+long long int fibonacci(int x);
+ 
+ int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long int fibonacci(int x){
+    if(x == 0){
+        return 0;
+    }
+    else if(x == 1){
+        return 1;
+    }
+    else{
+        return x = fibonacci(x-1) + fibonacci(x-2);
+    }
 }
